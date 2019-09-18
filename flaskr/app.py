5c80e543cf9f667a0263bb8f9a27b76aa7238025
Marshall McDonnell <mcdonnellmt@ornl.gov>
2019-09-18 00:20:59 -0400

    Added login html, fixed given errors for TDD

--- flaskr/app.py
+++ flaskr/app.py
@@ -47,7 +47,7 @@ def close_db(error):
         g.sqlite_db.close()
 
 @app.route('/')
-def show_entries():
+def index():
     """Searches the database for entries, then displays them."""
     db = get_db()
     cur = db.execute('select * from entries order by id desc')
@@ -75,6 +75,20 @@ def logout():
     """User logout/authentication/session management."""
     session.pop('logged_in', None)
     flash('You were logged out')
+    return redirect(url_for('index'))
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Add new post to database."""
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute(
+        'insert into entries (title, text) values (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted')
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
